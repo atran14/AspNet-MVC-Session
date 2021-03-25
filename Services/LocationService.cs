4cb2ab507135fa53ab94380767d7625043f6afc0
Doc1997 <anhtranduc1997@gmail.com>
2021-03-26 02:21:07 +0700

    Created location service

---
+++ Services/LocationService.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace AspNet_MVC_Session.Services
+{
+    public class LocationService
+    {
+        public List<string> GetLocationsList()
+        {
+            return new List<string>
+            {
+                "Hanoi",
+                "Thanh Hoa",
+                "Hai Phong",
+                "Nghe An",
+                "TP Ho Chi Minh"
+            };
+        }
+    }
+}
